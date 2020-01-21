02e6ef818f48902854b8d506da76520aadac7243
Zach Brown <zachmb22@gmail.com>
2020-01-21 02:00:47 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,11 +43,24 @@ def naming_system
   # HoH all the way to Species!
   
   bio = {
-    label: "Kingdom",
+   label: "Kingdom",
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class"
+       label: "Class",
+       sub_category: {
+         label: "Order",
+         sub_category: {
+           label: "Family",
+           sub_category: {
+            label: "Genus",
+            sub_category: {
+              label: "Species",
+              sub_category: nil
+            }
+           }
+         }
+       }
      }
    }
   }
